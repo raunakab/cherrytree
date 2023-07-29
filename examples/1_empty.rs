64054d5885da447fcaf4b0e28acbc7c173e7bdd9
Raunak Bhagat <rabhagat31@gmail.com>
2023-07-29 16:55:38 -0700

    Add some examples

---
+++ examples/1_empty.rs
@@ -0,0 +1,14 @@
+//! This example showcases how to create a default, empty [`Tree`] instance.
+//!
+//! The key that is being used to index into this [`Tree`] is the [`DefaultKey`]
+//! type that is provided by the [`slotmap`] crate. If you want to be able to
+//! use your own custom keys, consider using the [`slotmap::new_key_type`]
+//! macro.
+
+use pettree::Tree;
+use slotmap::DefaultKey;
+
+fn main() {
+    let tree = Tree::<DefaultKey, usize>::default();
+    assert!(tree.is_empty());
+}
---
+++ examples/2_add_root_and_children.rs
@@ -0,0 +1,54 @@
+//! This example showcases how to create a default, empty [`Tree`] instance and
+//! insert a root value and 3 children values into it. In this example, the
+//! children values will be *direct* children of the root value.
+
+use std::collections::BTreeSet;
+
+use pettree::Tree;
+use slotmap::DefaultKey;
+
+fn main() {
+    let mut tree = Tree::<DefaultKey, usize>::default();
+
+    let root_key = tree.insert_root(0);
+
+    let child_key_1 = tree.insert(root_key, 1).unwrap();
+    let child_key_2 = tree.insert(root_key, 2).unwrap();
+    let child_key_3 = tree.insert(root_key, 3).unwrap();
+
+    assert!(!tree.is_empty());
+
+    let root_node = tree.get(root_key).unwrap();
+    assert_eq!(*root_node.value, 0);
+    assert_eq!(root_node.parent_key, None);
+    assert_eq!(
+        root_node.child_keys.into_iter().collect::<BTreeSet<_>>(),
+        vec![child_key_1, child_key_2, child_key_3]
+            .into_iter()
+            .collect(),
+    );
+
+    let child_node_1 = tree.get(child_key_1).unwrap();
+    assert_eq!(*child_node_1.value, 1);
+    assert_eq!(child_node_1.parent_key, Some(root_key));
+    assert_eq!(
+        child_node_1.child_keys.collect::<Vec<_>>().len(),
+        0,
+    );
+
+    let child_node_2 = tree.get(child_key_2).unwrap();
+    assert_eq!(*child_node_2.value, 2);
+    assert_eq!(child_node_2.parent_key, Some(root_key));
+    assert_eq!(
+        child_node_2.child_keys.collect::<Vec<_>>().len(),
+        0,
+    );
+
+    let child_node_3 = tree.get(child_key_3).unwrap();
+    assert_eq!(*child_node_3.value, 3);
+    assert_eq!(child_node_3.parent_key, Some(root_key));
+    assert_eq!(
+        child_node_3.child_keys.collect::<Vec<_>>().len(),
+        0,
+    );
+}
