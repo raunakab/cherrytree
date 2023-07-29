dcab77ae9b34ff6dd3c884e2b99e48378bacbaa1
Raunak Bhagat <rabhagat31@gmail.com>
2023-07-29 14:53:28 -0700

    Finish (most of) the implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 use std::collections::BTreeMap;
 
 use petgraph::{
+    graphmap::Neighbors,
     prelude::GraphMap,
     Directed,
 };
 use slotmap::{
+    basic::{
+        Keys,
+        Values,
+        ValuesMut,
+    },
     Key,
     SlotMap,
 };
@@ -20,17 +26,26 @@ where
 }
 
 /// # Purpose:
+/// Checking/assertion methods.
+impl<K, V> Tree<K, V>
+where
+    K: Key,
+{
+    pub fn contains(&self, key: K) -> bool {
+        self.values.contains_key(key)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.values.is_empty()
+    }
+}
+
+/// # Purpose:
 /// Insertion/removal methods.
 impl<K, V> Tree<K, V>
 where
     K: Key,
 {
-    /// # Purpose:
-    /// Inserts a new root node into this [`Tree`].
-    ///
-    /// # Note:
-    /// In the case that this [`Tree`] already contained a root node, the entire
-    /// [`Tree`] is cleared and replaced with this new root node.
     pub fn insert_root(&mut self, value: V) -> K {
         if self.root_key.is_some() {
             self.clear();
@@ -43,21 +58,141 @@ where
         root_key
     }
 
-    pub fn try_insert(&mut self, parent_key: K, value: V) -> Option<K> {
-        todo!()
+    pub fn insert(&mut self, parent_key: K, value: V) -> Option<K> {
+        self.contains(parent_key).then(|| {
+            let key = self.values.insert(value);
+            self.up_map.insert(key, parent_key);
+            self.down_map.add_edge(parent_key, key, ());
+            key
+        })
     }
 
-    pub fn remove(&mut self, key: K) -> Option<V> {
-        todo!()
+    pub fn remove(&mut self, key: K, size_hint: Option<usize>) -> Option<V> {
+        let remove_single_forced = |tree: &mut Tree<_, _>, key| {
+            let value = tree.values.remove(key).unwrap();
+
+            tree.up_map.remove(&key);
+            tree.down_map.remove_node(key);
+
+            value
+        };
+
+        self.root_key.and_then(|root_key| {
+            if key == root_key {
+                let value = remove_single_forced(self, key);
+                self.clear();
+                Some(value)
+            } else {
+                self.descendent_keys(key, size_hint).map(|descendent_keys| {
+                    descendent_keys.iter().skip(1).for_each(|&descendent_key| {
+                        remove_single_forced(self, descendent_key);
+                    });
+                    remove_single_forced(self, key)
+                })
+            }
+        })
     }
 
-    /// # Purpose:
-    /// Clears the entire [`Tree`] instance of all nodes and edges. Keeps the
-    /// allocated memory for reuse.
     pub fn clear(&mut self) {
         self.values.clear();
         self.up_map.clear();
         self.down_map.clear();
         self.root_key = None;
     }
+}
+
+/// # Purpose:
+/// Getter/setter methods.
+impl<K, V> Tree<K, V>
+where
+    K: Key,
+{
+    pub fn len(&self) -> usize {
+        self.values.len()
+    }
+
+    pub fn keys(&self) -> Keys<'_, K, V> {
+        self.values.keys()
+    }
+
+    pub fn values(&self) -> Values<'_, K, V> {
+        self.values.values()
+    }
+
+    pub fn values_mut(&mut self) -> ValuesMut<'_, K, V> {
+        self.values.values_mut()
+    }
+
+    pub fn get(&self, key: K) -> Option<Node<'_, K, V>> {
+        self.values.get(key).map(|value| {
+            let parent_key = self.up_map.get(&key).copied();
+            let child_keys = self.down_map.neighbors(key);
+
+            Node {
+                parent_key,
+                value,
+                child_keys,
+            }
+        })
+    }
+
+    pub fn get_mut(&mut self, key: K) -> Option<NodeMut<'_, K, V>> {
+        self.values.get_mut(key).map(|value| {
+            let parent_key = self.up_map.get(&key).copied();
+            let child_keys = self.down_map.neighbors(key);
+
+            NodeMut {
+                parent_key,
+                value,
+                child_keys,
+            }
+        })
+    }
+
+    pub fn descendent_keys(&self, key: K, size_hint: Option<usize>) -> Option<Vec<K>> {
+        self.contains(key).then(|| {
+            let size_hint = size_hint.unwrap_or_else(|| self.len());
+
+            let mut to_visit_keys = Vec::with_capacity(size_hint);
+            let mut descendent_keys = Vec::with_capacity(size_hint);
+
+            to_visit_keys.push(key);
+
+            while let Some(to_visit_key) = to_visit_keys.pop() {
+                descendent_keys.push(to_visit_key);
+                let to_visit_child_keys = self.down_map.neighbors(to_visit_key);
+                to_visit_keys.extend(to_visit_child_keys);
+            }
+
+            descendent_keys
+        })
+    }
+}
+
+/// # Purpose:
+/// Default creation of a [`Tree`] instance.
+impl<K, D> Default for Tree<K, D>
+where
+    K: Key,
+{
+    fn default() -> Self {
+        Self {
+            values: SlotMap::default(),
+            up_map: BTreeMap::default(),
+            down_map: GraphMap::default(),
+            root_key: None,
+        }
+    }
+}
+
+pub struct Node<'a, K, V> {
+    pub parent_key: Option<K>,
+    pub value: &'a V,
+    pub child_keys: Neighbors<'a, K, Directed>,
+}
+
+pub struct NodeMut<'a, K, V> {
+    pub parent_key: Option<K>,
+    pub value: &'a mut V,
+    pub child_keys: Neighbors<'a, K, Directed>,
 }
