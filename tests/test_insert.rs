90ced9952f8e6ed302a24d77be59c8992d879c2b
Raunak Bhagat <rabhagat31@gmail.com>
2023-08-20 14:12:01 -0700

    Edit `test_insert` tests

--- tests/test_insert.rs
+++ tests/test_insert.rs
@@ -1,42 +1,67 @@
-// #[path = "../common/mod.rs"]
-// mod common;
-//
-// use slotmap::DefaultKey;
-//
-// #[test]
-// fn test_insert_with_random_parent_key() {
-//     let trees = common::fixtures::all();
-//
-//     for mut tree in trees {
-//         let key = DefaultKey::default();
-//         assert!(tree.insert(key, 0).is_none());
-//         assert!(!tree.contains(key));
-//     }
-// }
-//
-// #[test]
-// fn test_insert_many_children() {
-//     let trees = common::fixtures::all_non_empty();
-//
-//     const NUMBER_OF_CHILDREN_TO_INSERT: usize = 100;
-//
-//     for mut tree in trees {
-//         let key = common::utils::get_some_leaf_key(&tree);
-//
-//         assert!(tree.contains(key));
-//         assert_eq!(tree.get(key).unwrap().child_keys.len(), 0);
-//
-//         (0..NUMBER_OF_CHILDREN_TO_INSERT).for_each(|value| {
-//             tree.insert(key, value).unwrap();
-//         });
-//
-//         let child_keys = tree.get(key).unwrap().child_keys;
-//         assert_eq!(child_keys.len(), NUMBER_OF_CHILDREN_TO_INSERT);
-//         child_keys.iter().for_each(|&child_key| {
-//             let child_node = tree.get(child_key).unwrap();
-//
-//             assert!(child_node.child_keys.is_empty());
-//             assert_eq!(child_node.parent_key.unwrap(), key);
-//         });
-//     }
-// }
+#[path = "../common/mod.rs"]
+mod common;
+
+use common::{
+    make_deserial_node,
+    make_tree_and_key_map,
+    node,
+    DeserialNode,
+};
+
+#[test]
+fn test_insert() {
+    let tests = [
+        ((None, 0, 1), (None, false)),
+        ((Some(node! { 0 }), 2, 1), (Some(node! { 0 }), false)),
+        (
+            (Some(node! { 0 }), 0, 1),
+            (Some(node! { 0, [node! { 1 }] }), true),
+        ),
+        (
+            (
+                Some(node! {
+                    0,
+                    [
+                        node! { 1 },
+                        node! { 2, [node! { 4, [node! { 5 }] }] },
+                        node! { 3 },
+                    ],
+                }),
+                2,
+                6,
+            ),
+            (
+                Some(node! {
+                    0,
+                    [
+                        node! { 1 },
+                        node! { 2, [node! { 4, [node! { 5 }] }, node! { 6 }] },
+                        node! { 3 },
+                    ],
+                }),
+                true,
+            ),
+        ),
+    ];
+
+    for (
+        (deserial_node, parent_key, value_to_insert),
+        (expected_deserial_node, expected_did_insert),
+    ) in tests
+    {
+        let (mut tree, mut key_map) = make_tree_and_key_map(deserial_node.as_ref());
+
+        let parent_key = key_map.get(&parent_key).copied().unwrap_or_default();
+        let key = tree.insert(value_to_insert, parent_key);
+
+        if let Some(key) = key {
+            key_map.insert(value_to_insert, key);
+        };
+
+        let actual_deserial_node = make_deserial_node(&tree);
+        let actual_did_insert = key.is_some();
+
+        assert_eq!(actual_deserial_node, expected_deserial_node);
+        assert_eq!(actual_did_insert, expected_did_insert);
+    }
+}
